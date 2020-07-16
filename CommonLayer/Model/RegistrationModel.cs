f28dcf6a6190d43951417a1c410ad3362cb8b8fb
Sandhya Patil <sandhyapatil364@gmail.com>
2020-07-16 20:05:08 +0530

    Added: Database creation added

---
+++ CommonLayer/Model/RegistrationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class RegistrationModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string MobileNumber { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ RepositoryLayer/Context/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using CommonLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Context
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<RegistrationModel> Registrations { get; set; }
+    }
+}
