bc5b85752eda3dd797b1b28e25480ea778ac2dd1
Sandhya Patil <sandhyapatil364@gmail.com>
2020-07-19 13:02:29 +0530

    Added: Registration data null return bad request

--- SocialSiteAppTestCases/SocialAccountTestCases.cs
+++ SocialSiteAppTestCases/SocialAccountTestCases.cs
@@ -20,7 +20,7 @@ namespace SocialSiteAppTestCases
         IAccountRL accountRL;
         IConfiguration configuration;
 
-        public static DbContextOptions<AppDBContext> appDBContext {get;}
+        public static DbContextOptions<AppDBContext> appDBContext { get; }
 
         public static string sqlConnectionString = "server=LAPTOP-JQPITHJ9;Database=SocialSiteDB;Trusted_Connection=true; MultipleActiveResultSets = true";
 
@@ -31,15 +31,19 @@ namespace SocialSiteAppTestCases
 
         public SocialAccountTestCases()
         {
-              var context = new AppDBContext(appDBContext);
+            var context = new AppDBContext(appDBContext);
             accountRL = new AccountRL(context);
             accountBL = new AccountBL(accountRL);
             IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
             configurationBuilder.AddJsonFile("appsettings.json");
             configuration = configurationBuilder.Build();
         }
+
+        /// <summary>
+        /// given request for user registration valid data return ok object result
+        /// </summary>
         [Fact]
-        public void SignUp_User_Return_OkResult()
+        public void Given_Request_For_UserRegistration_ValidData_Return_OkResult()
         {
             var controller = new AccountController(accountBL, configuration);
             var data = new RegistrationShowModel()
@@ -54,7 +58,14 @@ namespace SocialSiteAppTestCases
             Assert.IsType<OkObjectResult>(response);
         }
 
-
+        [Fact]
+        public void Given_Request_For_UserReg_DataNotProvided_Return_BadRequest()
+        {
+            var controller = new AccountController(accountBL, configuration);
+            RegistrationShowModel data = null;
+            var response = controller.UserSignUp(data);
+            Assert.IsType<BadRequestObjectResult>(response);
+        }
     }
 }
     
